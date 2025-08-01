20df082bac4beb478c56f51979e77a926b1a09ce
ReneLaverdure <renelaverdure@gmail.com>
2025-08-01 16:18:35 +1000

    init commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function main() {
+    console.log("this is the main function");
+}
+main();
+//# sourceMappingURL=index.js.map
